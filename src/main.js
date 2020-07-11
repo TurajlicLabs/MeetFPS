57273df9c58b9667d78a1df71985357b6c815265
Dusan Turajlic <dusan@turajlic.fi>
2020-07-11 19:52:57 +0300

    Bunch of hacks and stuff for babylon and Jitsi

--- src/main.js
+++ src/main.js
@@ -4,6 +4,31 @@ import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+const $global = Vue.prototype.$global = window;
+const $doc = Vue.prototype.$doc = document;
+// polyfill for jQuary so that `lib-jitsi-meet/modules/xmpp/xmpp.js` can work properly
+$global.$ = () => {
+    const jQuaryPolyfill = {
+        on( events, handler ) {
+            events.split( ' ' ).forEach( ( event ) => {
+                $global.addEventListener( event, handler );
+            } );
+        }
+    };
+
+    jQuaryPolyfill.find = ( selector ) => {
+        let elements = $doc.querySelectorAll( selector );
+        if ( elements.length > 1 ) {
+            return elements;
+        }
+        else {
+            // in case long find chains are being used.
+            return Object.assign( elements[0], jQuaryPolyfill );
+        }
+    };
+
+    return jQuaryPolyfill;
+};
 
 new Vue( {
     router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,54 @@
 <template>
-    <div class="home"></div>
+    <div class="home">
+        <canvas @resize="engine.resize()" ref="canvas"></canvas>
+    </div>
 </template>
 
 <script>
+    import Engine from '@/engine';
+    /* eslint-disable */
+    const { JitsiConnection } = JitsiMeetJS;
+
+    const ROOM = 'superspecificroomfortesting';
+    // const BOSH_URL = `"//alpha.jitsi.net/http-bind?room=${ ROOM }`;
+
     export default {
-        name: 'Home',
+        name : 'Home',
+        data() {
+            return {
+                engine : null
+            }
+        },
+        async mounted() {
+            let canvas = this.$refs[ 'canvas' ];
+            this.engine = await Engine( canvas, this );
+            this.connect();
+        },
+        methods : {
+            async connect() {
+                let configModule = await import( '@/config/jitisConfig' );
+                const config = configModule.default;
+                let domain = config.hosts.domain;
+                config.serverUrl = config.bosh = `/http-bind?room=${ROOM}`;
+
+                let jConnect = new JitsiConnection( null, '', config );
+                console.log( jConnect );
+                let response = await fetch( `/http-pre-bind?room=${ROOM}` );
+                let options = JSON.parse( await response.text() );
+                jConnect.attach( options );
+            }
+        }
     }
 </script>
+
+<style scoped>
+    canvas {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
